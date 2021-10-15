52cd4f41affffc20f84aa2a5b6902f08cb613393
Yaroslav <garbar.jarek@gmail.com>
2021-10-15 19:03:20 +0200

    first ex

---
+++ EX1/first.py
@@ -0,0 +1,22 @@
+import time
+
+
+def timeDekorator(function):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        answer = function(*args, **kwargs)
+        end = time.time()
+
+        print(f"Executing time: {(end- start)*1000}")
+        return answer
+    return wrapper
+
+
+@timeDekorator
+def testFunction(firstNumber, secondNumber):
+    #time.sleep(1) # if time of executing is 0 then you have cool computer, than uncomment this line
+    return firstNumber + secondNumber
+
+
+if __name__ == "__main__":
+    print(testFunction(2, 3))
